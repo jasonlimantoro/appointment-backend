b709224bf81d8d3aee8839372915ba2ac6646f40
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-06-07 21:50:28 +0900

    CONFIG(eslint): add eslint

--- handler.js
+++ handler.js
@@ -1,14 +1,16 @@
-export const hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
-    }),
-  };
-};
+/* eslint-disable no-unused-vars */
+const hello = async (event, context) => ({
+  statusCode: 200,
+  body: JSON.stringify({
+    message: `Go Serverless v1.0! ${await message({
+      time: 1,
+      copy: 'Your function executed successfully!',
+    })}`,
+  }),
+});
 
-const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
-  setTimeout(() => {
-    resolve(`${rest.copy} (with a delay)`);
-  }, time * 1000)
-);
+const message = ({ time, ...rest }) => new Promise(resolve => setTimeout(() => {
+  resolve(`${rest.copy} (with a delay)`);
+}, time * 1000));
+
+export default hello;
