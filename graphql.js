ef2ffdd96d54d45c0bf92617324d2c06a1a8cffb
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-06-11 22:22:08 +0700

    REFACTOR(service): use service

--- graphql.js
+++ graphql.js
@@ -1,11 +1,15 @@
 import { ApolloServer } from 'apollo-server-lambda';
 import { resolvers, typeDefs } from './schema';
+import GuestService from './services/guest';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   formatError: error => error,
   formatResponse: response => response,
+  dataSources: () => ({
+    GuestAPI: new GuestService(),
+  }),
 });
 
 const hello = server.createHandler({
--- schema/guest/resolvers.js
+++ schema/guest/resolvers.js
@@ -1,34 +1,7 @@
-const guests = [
-  {
-    id: 'ah',
-    firstName: 'Andy',
-    lastName: 'Huang',
-    email: 'andy@gmail.com',
-    NIK: '12312365',
-    company: 'IMCP',
-  },
-  {
-    id: 'bh',
-    firstName: 'Budy',
-    lastName: 'Harjo',
-    email: 'budy@gmail.com',
-    NIK: '12832837',
-    company: 'Google',
-  },
-  {
-    id: 'cp',
-    firstName: 'Charlie',
-    lastName: 'Putt',
-    email: 'charlie@gmail.com',
-    NIK: '128373987',
-    company: 'Facebook',
-  },
-];
-
 const resolvers = {
   Query: {
-    listGuest: () => guests,
-    getGuest: (_, args) => guests.find(g => g.id === args.id),
+    listGuest: (_source, _args, context) => context.dataSources.GuestAPI.list(),
+    getGuest: (_source, args, context) => context.dataSources.GuestAPI.get(args.id),
   },
 };
 
