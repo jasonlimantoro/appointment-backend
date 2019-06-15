72df20c073cdfad5e43cb7374e7722d919088482
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-06-15 12:43:08 +0700

    FEAT(appsync): add appsync
    - Currently appsync doesn't support extend keyword in graphgql.

--- schema/index.js
+++ schema/index.js
@@ -1,20 +1,8 @@
-import gql from 'graphql-tag';
 import { merge } from 'lodash';
 import { makeExecutableSchema } from 'graphql-tools';
+import root from './root.graphql';
 import { resolvers as entryResolver, typeDefs as entryTypeDefs } from './entry';
 import { resolvers as guestResolver, typeDefs as guestTypeDefs } from './guest';
-
-const root = gql`
-	type Query {
-		root: String
-	}
-	type Mutation {
-		root: String
-	}
-	type Subscription {
-		root: String
-	}
-`;
 
 const typeDefs = [root, entryTypeDefs, guestTypeDefs];
 const resolvers = merge(entryResolver, guestResolver);
