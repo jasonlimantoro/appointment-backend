92fd6a76454209305c611ecf2836203ffa428f05
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-06-19 22:16:33 +0700

    CHORE(scripts): remove scripts from repo for now

--- scripts/confirm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-aws cognito-idp admin-confirm-sign-up \
-  --region us-east-1 \
-  --user-pool-id us-east-1_Pv8Y1sOqQ \
-  --username "dummy-staff"
--- scripts/signup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-aws cognito-idp sign-up \
-  --region us-east-1 \
-  --client-id 3lb5e2q7uoap9shdc3skta638n \
-  --username "dummy-staff" \
-  --password "dummy&Staff"
