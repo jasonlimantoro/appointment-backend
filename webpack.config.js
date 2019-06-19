b0b14f9cee7a8481fef01ce6d76ead44d412c004
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-06-20 02:16:22 +0700

    FIX: add polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: slsw.lib.entries,
+  entry: {
+    graphql: ['./libs/polyfill.js', './graphql.js'],
+  },
   target: 'node',
   // Generate sourcemaps for proper error messages
   devtool: 'source-map',
