2e1c2477125bbb77f6b60cc62484ce58a6dc32ba
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-06-21 20:54:15 +0700

    FIX(datetime): need to offset the month index

--- libs/datetime.js
+++ libs/datetime.js
@@ -2,7 +2,7 @@ const pad = n => (n < 10 ? `0${n}` : n);
 
 export const humanFormat = obj => {
   const y = obj.getFullYear();
-  const mon = pad(obj.getMonth());
+  const mon = pad(obj.getMonth() + 1);
   const d = pad(obj.getDate());
   const h = pad(obj.getHours());
   const min = pad(obj.getMinutes());
