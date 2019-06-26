4a381f1da2ec1ad3ad74508c35e45b9fcb15d714
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-06-26 12:09:25 +0700

    CHORE(fixture): add more user

--- fixtures/users.js
+++ fixtures/users.js
@@ -1,4 +1,10 @@
-module.exports = {
-  username: 'dummy-staff',
-  password: 'dummy&Staff',
-};
+module.exports = [
+  {
+    username: 'dummy-staff',
+    password: 'dummy&Staff',
+  },
+  {
+    username: 'dummy-staff-1',
+    password: 'dummy&Staff1',
+  },
+];
