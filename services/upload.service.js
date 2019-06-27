7561da49fa141549ad8d4d5b80d98dc481388386
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-06-27 19:46:26 +0700

    FIX(service): fix filename format

--- services/upload.service.js
+++ services/upload.service.js
@@ -1,24 +1,22 @@
 import { S3 } from 'aws-sdk';
-import Auth from '@aws-amplify/auth';
-import { getNestedObjectValue } from 'appointment-common';
 import BaseService from './base';
 import AWSConfiguration from '../config/aws-exports';
+import { humanFormat } from '../libs/datetime';
 
 export default class UploadService extends BaseService {
-  sign = async ({ fileName, fileType }) => {
-    const res = await Auth.currentUserInfo();
-    const sub = getNestedObjectValue(res)(['attributes', 'sub']);
-    const uniqueFileName = `${new Date().toISOString()}-${fileName}`;
+  sign = async ({ fileName: filePath, fileType }) => {
+    const uniqueFileName = `${humanFormat(new Date())}`;
     const s3 = new S3();
     const params = {
       Bucket: AWSConfiguration.Storage.AWSS3.bucket,
-      Key: `${sub}/${uniqueFileName}`,
+      Key: `${filePath}/${uniqueFileName}`,
       ContentType: fileType,
     };
     const signedRequest = await s3.getSignedUrl('putObject', params);
     const url = await s3.getSignedUrl('getObject', {
       Bucket: params.Bucket,
       Key: params.Key,
+      Expires: 30,
     });
     return {
       signedRequest,
