68e8e86bee316e33b9ef0c7f91ca09f5c13a946d
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-08-02 00:46:53 +0800

    REFACTOR: remove unused import + reformatting

--- schema/guest/resolvers.js
+++ schema/guest/resolvers.js
@@ -1,46 +1,53 @@
-import { filterToday, checkAuthentication } from '../../libs/resolverUtils';
+import { checkAuthentication } from '../../libs/resolverUtils';
 
 const resolvers = {
   Query: {
-    listGuest: (_source, _args, context) => checkAuthentication(
-      context,
-      async () => context.dataSources.guestAPI.list(),
-      'DynamoDB.DocumentClient',
-      service => {
-        context.dataSources.guestAPI.replaceDataSource(service);
-      },
-    ),
-    getGuest: (_source, args, context) => checkAuthentication(
-      context,
-      async () => {
-        const res = await context.dataSources.guestAPI.get(args.NIK);
-        return res;
-      },
-      'DynamoDB.DocumentClient',
-      service => {
-        context.dataSources.guestAPI.replaceDataSource(service);
-      },
-    ),
-    byName: (_source, args, context) => checkAuthentication(
-      context,
-      async () => context.dataSources.guestAPI.byName(args.input),
-      'DynamoDB.DocumentClient',
-      service => {
-        context.dataSources.guestAPI.replaceDataSource(service);
-      },
-    ),
+    listGuest: (_source, _args, context) =>
+      checkAuthentication(
+        context,
+        async () => context.dataSources.guestAPI.list(),
+        'DynamoDB.DocumentClient',
+        service => {
+          context.dataSources.guestAPI.replaceDataSource(service);
+        },
+      ),
+    getGuest: (_source, args, context) =>
+      checkAuthentication(
+        context,
+        async () => {
+          const res = await context.dataSources.guestAPI.get(args.NIK);
+          return res;
+        },
+        'DynamoDB.DocumentClient',
+        service => {
+          context.dataSources.guestAPI.replaceDataSource(service);
+        },
+      ),
+    byName: (_source, args, context) =>
+      checkAuthentication(
+        context,
+        async () => context.dataSources.guestAPI.byName(args.input),
+        'DynamoDB.DocumentClient',
+        service => {
+          context.dataSources.guestAPI.replaceDataSource(service);
+        },
+      ),
   },
   Mutation: {
-    createGuest: (_source, args, context) => checkAuthentication(
-      context,
-      async () => context.dataSources.guestAPI.create(args.input),
-      'DynamoDB.DocumentClient',
-      service => {
-        context.dataSources.guestAPI.replaceDataSource(service);
-      },
-    ),
+    createGuest: (_source, args, context) =>
+      checkAuthentication(
+        context,
+        async () => context.dataSources.guestAPI.create(args.input),
+        'DynamoDB.DocumentClient',
+        service => {
+          context.dataSources.guestAPI.replaceDataSource(service);
+        },
+      ),
   },
   Guest: {
+    /**
+     * @todo fix filtering
+     */
     entryToday: async (source, _args, { dataSources }) => {
       const res = await dataSources.entryAPI.byGuestId({
         id: source.getDataValue('NIK'),
