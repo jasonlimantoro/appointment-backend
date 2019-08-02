46ef08a2de49e1635ade04bd9147e695ddf81afc
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-08-02 23:06:05 +0800

    FIX(pagination): do fixes
    - remove reference of Count and Items from DynamoDB
    - pagination still needs some fixes
    - reformatting

--- libs/resolverUtils.js
+++ libs/resolverUtils.js
@@ -78,18 +78,19 @@ export const checkAuthGroup = async (
   expectedGroups,
   controller,
   ...params
-) => checkAuthentication(context, async user => {
-  const groups = user['cognito:groups'];
-  if (!groups) {
-    throw new AuthorizationError('No group is attached');
-  }
-  if (groups && expectedGroups.some(group => !groups.includes(group))) {
-    throw new AuthorizationError(
-      `You are not authorized. Expected scopes: ${expectedGroups.join(', ')}`,
-    );
-  }
-  return controller.apply(this, [...params, user, context]);
-});
+) =>
+  checkAuthentication(context, async user => {
+    const groups = user['cognito:groups'];
+    if (!groups) {
+      throw new AuthorizationError('No group is attached');
+    }
+    if (groups && expectedGroups.some(group => !groups.includes(group))) {
+      throw new AuthorizationError(
+        `You are not authorized. Expected scopes: ${expectedGroups.join(', ')}`,
+      );
+    }
+    return controller.apply(this, [...params, user, context]);
+  });
 
 /**
  * @deprecated
@@ -114,7 +115,8 @@ export const filterToday = list => {
   const later = moment()
     .hours(23)
     .minutes(59);
-  return list.filter(({ createdAt }) => moment(createdAt).isBetween(midnight, later));
+  return list.filter(({ createdAt }) =>
+    moment(createdAt).isBetween(midnight, later));
 };
 
 /**
@@ -122,27 +124,30 @@ export const filterToday = list => {
  * @param {object} context the context object sent
  * @returns {string}
  */
-export const getLambdaRequestId = context => getNestedObjectValue(context)(['event', 'requestContext', 'requestId']);
+export const getLambdaRequestId = context =>
+  getNestedObjectValue(context)(['event', 'requestContext', 'requestId']);
 
 /**
  * To retrieve the api request ID
  * @param {object} context the context object sent
  * @returns {string}
  */
-export const getAPIRequestId = context => getNestedObjectValue(context)(['context', 'awsRequestId']);
+export const getAPIRequestId = context =>
+  getNestedObjectValue(context)(['context', 'awsRequestId']);
 
-export const constructFileName = ({ prefix = '', fileName, fileType } = {}) => `${prefix}/${humanFormat(new Date())}-${fileName}.${mime.extension(
-  fileType,
-)}`;
+export const constructFileName = ({ prefix = '', fileName, fileType } = {}) =>
+  `${prefix}/${humanFormat(new Date())}-${fileName}.${mime.extension(
+    fileType,
+  )}`;
 
 export const paginate = (res, itemToCursor) => {
-  const edges = res.Items.map(item => ({
+  const edges = res.map(item => ({
     node: item,
     cursor: itemToCursor(item),
   }));
   const pagination = {
     hasNext: !!res.LastEvaluatedKey,
-    count: res.Count,
+    count: res.length,
   };
   return {
     edges,
